bc663de4abf2ef378a57f93ebb0c5226a1ac8b1d
JayaramRCDAC <jayaram.r@cdac.in>
2024-04-11 15:47:31 +0530

    Changed the task operator to original

--- core/task.cc
+++ core/task.cc
@@ -47,6 +47,7 @@ void Task::Attach(bess::LeafTrafficClass *c) {
   c_ = c;
 }
 
+/*
 struct task_result Task::operator()(Context *ctx) const {
   bess::PacketBatch init_batch;
   ClearPacketBatch();
@@ -68,6 +69,51 @@ struct task_result Task::operator()(Context *ctx) const {
     if (!batch) {
       // Skip processing empty batches
       continue;
+    }
+
+    ctx->current_igate = igate->gate_idx();
+
+    for (auto &hook : igate->hooks()) {
+      hook->ProcessBatch(batch);
+    }
+
+    Module *m = igate->module();
+    m->ProcessBatch(ctx, batch);  // process module
+    m->ProcessOGates(ctx);        // process ogates
+  }
+
+  deadend(ctx, &dead_batch_);
+
+  return result;
+}*/
+
+struct task_result Task::operator()(Context *ctx) const {
+  bess::PacketBatch init_batch;
+  ClearPacketBatch();
+
+  // Start from the first module (task module)
+  struct task_result result = module_->RunTask(ctx, &init_batch, arg_);
+  // next_gate_: Continuously run if modules are chained
+  // igates_to_run_ : If next module connection is not chained (merged),
+  // check priority to choose which module run next
+  while (next_gate_ || !igates_to_run_.empty()) {
+    bess::IGate *igate;
+    bess::PacketBatch *batch;
+
+    // choose igate and batch to run next
+    if (next_gate_) {
+      igate = next_gate_;
+      batch = next_batch_;
+      next_gate_ = nullptr;
+      next_batch_ = nullptr;
+    } else {
+      auto item = igates_to_run_.top();
+      igates_to_run_.pop();
+
+      igate = item.first;
+      batch = item.second;
+
+      set_gate_batch(igate, nullptr);
     }
 
     ctx->current_igate = igate->gate_idx();
